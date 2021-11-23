43e0b337263f936bafacee6b65b4b59b24943c5a
João Henrique <79462403+ccojoaolima@users.noreply.github.com>
2021-11-23 10:51:37 -0300

    scriptDokcer upado

---
+++ scriptDocker.sql
@@ -0,0 +1,9 @@
+create database informacoesDosJogadores;
+use informacoesDosJogadores;
+create table estatisticas(
+idJogador int primary key auto_increment,
+nomeInvocador varchar(30),
+campeaoPrincipal varchar(30),
+winrate int
+);
+
